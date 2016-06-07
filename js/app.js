2aa5fc3abfb3515975bfa1504a4a2eb76e325a24
Lyn Dee <dee.shirlyn@gmail.com>
2016-06-07 13:53:02 -0400

    moved functions out of jquery onload function to make the app.js file prettier

--- js/app.js
+++ js/app.js
@@ -1,13 +1,18 @@
 $( document ).ready(function() {
-
-$('div').each(function(index) {
-
-	var $idValue = $( this ).attr('id');
-
-	$(this).html($idValue)
-
+getDivs();
 
 });
 
 
-});
+function getDivs () {
+	$('div').each(function(index) {
+
+		var $idValue = $( this ).attr('id'); //Gets the id of the div
+		$(this).html($idValue) //outputs it on html so that I can see the id of the div
+
+		$(this).on("click", function(e) {
+			alert($idValue)
+		})
+
+	});
+}
