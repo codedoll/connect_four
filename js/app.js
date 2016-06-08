e77302f4a3351d58a6a2c408a09367e42d39aa0c
Lyn Dee <dee.shirlyn@gmail.com>
2016-06-07 20:33:28 -0400

    user is able to choose the tile that is unclicked nearest to the bottom

--- js/app.js
+++ js/app.js
@@ -1,18 +1,75 @@
-$( document ).ready(function() {
-getDivs();
+$(document).ready(function() {
 
-});
+    makeDivs();
 
 
-function getDivs () {
-	$('div').each(function(index) {
+})
 
-		var $idValue = $( this ).attr('id'); //Gets the id of the div
-		$(this).html($idValue) //outputs it on html so that I can see the id of the div
 
-		$(this).on("click", function(e) {
-			alert($idValue)
-		})
+function makeDivs() {
 
-	});
+    $('div').each(function(index) {
+        var $idValue = $(this).attr('id'); //Gets the id of the div
+        $(this).html($idValue) //outputs it on html so that I can see the id of the div
+        $(this).addClass('noclick')
+
+        // 	$(this).on("click", function(e) {
+        // 		$(this).addClass('clicked')
+        // });
+
+
+
+    })
+    activateTop();
 }
+
+
+function activateTop() {
+
+    console.log($("div[id*='6']"))
+    var $a = $("div[id*='6']");
+    $($a).each(function(index) {
+        $(this).removeClass('noclick')
+    })
+
+    nowClickable();
+
+}
+
+function nowClickable() {
+    var $this;
+    $('div').on("click", function(e) {
+
+        $(this).addClass('clicked')
+        $this = $(this).attr('id')
+        highlightGuyOnBottom($this);
+
+
+    });
+
+}
+
+function highlightGuyOnBottom($this) {
+    var arr = [];
+    console.log($this)
+
+    var $oneLetter = $this.slice(0, -1)
+
+    $(`div[id*='${$oneLetter}']`).each(function(index) {
+
+        console.log($(this))
+
+        if (!$(this).hasClass("clicked")) {
+            arr.push($(this).attr('id'));
+
+        }
+
+    })
+
+    console.log(arr)
+
+    console.log(arr[arr.length - 1])
+    var lastElement = arr[arr.length - 1]
+    $(`#${lastElement}`).addClass('clickable')
+}
+
